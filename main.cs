a0edc73aaffbe24b067e55e8c7ff48493c34e784
Carlos Avila <35896114-cavila12@users.noreply.replit.com>
2024-09-15 14:32:57 +0000

    2

--- main.cs
+++ main.cs
@@ -4,6 +4,10 @@ class InchesToCentimeters
 {
   static void Main (string[] args)
   {
-    
+
+    const double CENTIMETERS_PER_INCH = 2.54;
+    double inches = 3;
+    Console.Write("Enter the number of inches: ")
+    inches = Convert.ToDouble(Console.ReadLine());
   }
 }
