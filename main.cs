51e185de1ae9ce08548f92b692c6063edcc58986
Carlos Avila <35896114-cavila12@users.noreply.replit.com>
2024-09-15 14:36:05 +0000

    2

--- main.cs
+++ main.cs
@@ -7,7 +7,9 @@ class InchesToCentimeters
 
     const double CENTIMETERS_PER_INCH = 2.54;
     double inches = 3;
-    Console.Write("Enter the number of inches: ")
+    Console.Write("Enter the number of inches: ");
     inches = Convert.ToDouble(Console.ReadLine());
+    double centimeters = inches * CENTIMETERS_PER_INCH;
+    Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);
   }
 }
